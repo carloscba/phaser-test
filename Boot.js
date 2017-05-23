37fe5c9735cb25b1cc122bfc415dadce6fc3056a
Carlos Cordoba <carlos.cordoba@m8agency.com>
2017-05-23 11:59:23 -0300

    - Preloader configurado

---
+++ Boot.js
@@ -0,0 +1,30 @@
+var gameBall = {};
+
+gameBall.Boot = function(game){};
+
+gameBall.Boot.prototype = {
+    preload : function(){
+        this.load.image('loader', './images/loader.png');
+        this.load.image('title-background', './images/title-background.jpg');
+        this.load.image('title-text', './images/title-text.jpg');
+        this.load.image('ball', './images/ball.png');
+        this.load.image('botin', './images/botin.png');        
+    },
+
+    create : function(){
+        this.input.maxPointers = 1;
+        this.stage.disableVisibilityChange = false;
+        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        this.scale.minWidth = 270;
+        this.scale.minHeight = 480;
+        this.scale.pageAlignHorizontall = true;
+        this.scale.pageAlignVertically = true;
+        this.scale.forcePortrait = true;
+        this.scale.setScreenSize = true;
+
+        this.input.addPointer();
+        this.stage.backgroundColor = '#000000';
+
+        this.state.start('Preloader');
+    }
+};
---
+++ Preloader.js
@@ -0,0 +1,26 @@
+gameBall.Preloader = function(game){
+    this.preloadBar = null;
+    this.titleText = null;
+    this.ready = false;
+};
+
+gameBall.Preloader.prototype = {
+
+    preload: function(){
+        this.preloadBar = this.add.sprite(this.world.centerX, this.world.centerY, 'loader');
+        this.preloadBar.anchor.setTo(0.5, 0.5);
+        this.load.setPreloadSprite(this.preloadBar);
+        
+        this.titleText = this.add.image(this.world.centerX, this.world.centerY - 200, 'title-text');
+        this.titleText.anchor.setTo(0.5, 0.5);
+    },
+
+    create: function(){
+        this.preloadBar.cropEnabled = false;
+    },    
+
+    update: function(){
+        this.ready = true;
+    },    
+
+}
